5e00a8cfc6d9a6306d58cab94e4cffdcc0e7afcb
kaige <kaige@biomap.com>
2021-10-26 22:59:40 +0800

    add beian

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { Provider as StyletronProvider } from "styletron-react";
-import { Server, Sheet } from "styletron-engine-atomic";
-import { styletron } from "../lib/styletron";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { Server, Sheet } from 'styletron-engine-atomic'
+import { styletron } from '../lib/styletron'
 
 class MyDocument extends Document<{ stylesheets: Sheet[] }> {
   static getInitialProps(props: any) {
@@ -9,9 +9,9 @@ class MyDocument extends Document<{ stylesheets: Sheet[] }> {
       <StyletronProvider value={styletron}>
         <App {...props} />
       </StyletronProvider>
-    ));
-    const stylesheets = (styletron as Server).getStylesheets() || [];
-    return { ...page, stylesheets };
+    ))
+    const stylesheets = (styletron as Server).getStylesheets() || []
+    return { ...page, stylesheets }
   }
 
   render() {
@@ -23,7 +23,7 @@ class MyDocument extends Document<{ stylesheets: Sheet[] }> {
               className="_styletron_hydrate_"
               dangerouslySetInnerHTML={{ __html: sheet.css }}
               media={sheet.attrs.media}
-              data-hydrate={sheet.attrs["data-hydrate"]}
+              data-hydrate={sheet.attrs['data-hydrate']}
               key={i}
             />
           ))}
@@ -32,10 +32,31 @@ class MyDocument extends Document<{ stylesheets: Sheet[] }> {
         <body>
           <Main />
           <NextScript />
+
+          <footer
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              paddingBottom: '24px',
+              textAlign: 'center',
+            }}
+          >
+            <a
+              href="https://beian.miit.gov.cn/"
+              style={{
+                fontSize: '14px',
+                color: '#0085ff',
+              }}
+            >
+              京ICP备19054223号-1
+            </a>
+          </footer>
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
